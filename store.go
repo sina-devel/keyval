8c3123b09cc402363ae54fbad4b4afe42423bd03
sina safari <sina.devel@gmail.com>
2022-01-18 00:32:09 +0330

    Add `Store` interface and `MemoryStore`

---
+++ store.go
@@ -0,0 +1,90 @@
+package keyval
+
+import (
+	"errors"
+	"sync"
+)
+
+var (
+	// ErrNotExist returns when the key is not exist.
+	ErrNotExist = errors.New("key is not exist")
+)
+
+// Store is an interface that Keyval uses to work on data.
+type Store interface {
+	// Put puts the value with the given key.
+	Put(key string, val []byte) error
+
+	// Get gets the key and returns the value.
+	//
+	// note: if key is not exist should return the ErrNotExist.
+	Get(key string) ([]byte, error)
+
+	// Drop drops the given key.
+	Drop(key string) error
+
+	// Keys returns all the stored keys.
+	Keys() []string
+}
+
+// MemoryStore implements Store interface.
+type MemoryStore struct {
+	mu   sync.Mutex
+	data map[string][]byte
+}
+
+// NewMemoryStore returns an initialized MemoryStore.
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		data: make(map[string][]byte),
+	}
+}
+
+// Put puts the value with the given key.
+//
+// note: if the key already exists, it overwrites it.
+func (m *MemoryStore) Put(key string, val []byte) error {
+	m.mu.Lock()
+	m.data[key] = val
+	m.mu.Unlock()
+	return nil
+}
+
+// Get gets the key and returns the value.
+func (m *MemoryStore) Get(key string) ([]byte, error) {
+	m.mu.Lock()
+
+	if val, ok := m.data[key]; ok {
+		return val, nil
+	}
+
+	m.mu.Unlock()
+	return nil, ErrNotExist
+}
+
+// Drop drops the given key.
+func (m *MemoryStore) Drop(key string) error {
+	m.mu.Lock()
+
+	if _, ok := m.data[key]; ok {
+		delete(m.data, key)
+		m.mu.Unlock()
+		return nil
+	}
+
+	m.mu.Unlock()
+	return ErrNotExist
+}
+
+// Keys returns all the stored keys.
+func (m *MemoryStore) Keys() []string {
+	m.mu.Lock()
+
+	var keys []string
+	for k := range m.data {
+		keys = append(keys, k)
+	}
+
+	m.mu.Unlock()
+	return keys
+}
